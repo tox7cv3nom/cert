bbb2c1985217a6514c45ec6ec8ab1649ca2c7bf4
toxic v3nom <nhibtaungamain@gmail.com>
2022-10-02 18:00:35 +0530

    Update cert.sh

--- cert.sh
+++ cert.sh
@@ -2,7 +2,7 @@
 echo "---------------------"
 echo "finding subdomains  "
 echo "---------------------"
-#python3 cert.py $1 | jq '' | awk '{print $2}' | grep "$1" | tr -d '",'| ./httprobe | sort -u | tee -a sub.txt
+python3 cert.py $1 | jq '' | awk '{print $2}' | grep "$1" | tr -d '",'| ./httprobe | sort -u | tee -a sub.txt
 echo "-----------------------------"
 echo subdomains saved to $(pwd)/sub2.txt
 echo "-----------------------------"
